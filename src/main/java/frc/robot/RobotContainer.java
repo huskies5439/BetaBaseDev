494e751f04c1797e0c68b26322943dd94a3d8bee
Huskies Prog <huskies5439@polych.ca>
2021-10-20 19:45:07 -0400

    -Teleop Base Pilotable et Brake (en booléen)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,11 +4,11 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.robot.subsystems.BasePilotable;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -18,14 +18,18 @@ import edu.wpi.first.wpilibj2.command.Command;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+ BasePilotable basePilotable = new BasePilotable();
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+ XboxController joystick = new XboxController(0);
+
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
+
+basePilotable.setDefaultCommand(new RunCommand(() -> basePilotable.conduire(joystick.getY(Hand.kLeft), joystick.getX(Hand.kRight)), basePilotable));
+
   }
 
   /**
@@ -43,6 +47,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return null;
   }
 }
---
+++ src/main/java/frc/robot/subsystems/BasePilotable.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class BasePilotable extends SubsystemBase {
+  /** Creates a new BasePilotable. */
+
+private WPI_TalonSRX moteurGauche = new WPI_TalonSRX(16);
+private WPI_TalonSRX moteurDroit = new WPI_TalonSRX(15);
+
+private DifferentialDrive drive = new DifferentialDrive(moteurGauche, moteurDroit);
+
+// TO DO : MODE RAMP ET AUTONOMOUS :D
+  public BasePilotable() {
+    moteurDroit.setInverted(true);
+    moteurGauche.setInverted(true);
+    setBrake(true);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+public void conduire(double vx,double vz) {
+  // Le vx en négatif car joystick en mode avion pis le vz capped a 70% de vitesse :D -Steven
+
+  drive.arcadeDrive(-vx, 0.7 * vz);
+}
+public void setBrake(boolean isBrake) {
+  if (isBrake) {
+    moteurDroit.setNeutralMode(NeutralMode.Brake);
+    moteurGauche.setNeutralMode(NeutralMode.Brake);
+  }
+  else {
+    moteurDroit.setNeutralMode(NeutralMode.Coast);
+    moteurGauche.setNeutralMode(NeutralMode.Coast);
+  }
+}
+
+}
