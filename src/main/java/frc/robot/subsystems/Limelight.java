01f46c2acbe1bde114988ac423401973483bee90
Huskies Prog <huskies5439@polych.ca>
2022-02-22 12:30:47 -0500

    Limelight
    P-A

---
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -0,0 +1,78 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Limelight extends SubsystemBase {
+  private NetworkTableInstance networkTableInstance = NetworkTableInstance.getDefault();
+  private NetworkTable limelight = networkTableInstance.getTable("limelight-huskies");
+  private NetworkTableEntry tv = limelight.getEntry("tv");
+  private NetworkTableEntry tx = limelight.getEntry("tx");
+  private NetworkTableEntry ta = limelight.getEntry("ta");
+  private NetworkTableEntry ty = limelight.getEntry("ty");
+  private NetworkTableEntry ledMode = limelight.getEntry("ledMode");
+  private NetworkTableEntry camMode = limelight.getEntry("camMode");
+
+
+
+  //Estimateur de distance, voir documentation Limelight
+  double hLimelight = 20.5; //pouce
+  double hCible = 89.5; //pouce
+  double angleLimelight=22.0;//degres
+  /**
+   * Creates a new Limelight.
+   */
+  public Limelight() {
+    ledOff();
+    camHumain();
+  }
+  public double getDistance(){
+    return(hCible-hLimelight)/(Math.tan(Math.toRadians(angleLimelight+getTy())));//donne la distance en pouces
+  }
+
+  public double getTa() {
+    return ta.getDouble(0);
+  }
+
+  public boolean getTv() {
+    return tv.getDouble(0)==1;
+  }
+
+  public double getTx() {
+    return tx.getDouble(0);
+  }
+
+  public double getTy() {
+    return ty.getDouble(0);
+  }
+
+  public void ledOn() {
+    ledMode.setNumber(3);
+  }
+  public void ledOff() {
+    ledMode.setNumber(1);
+  }
+  public void camHumain(){
+    camMode.setNumber(1);
+  }
+  public void camDetection(){
+    camMode.setNumber(0);
+  }
+ 
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Ta", getTa());
+    SmartDashboard.putNumber("tx", getTx());
+    SmartDashboard.putNumber("ty", getTy());
+    SmartDashboard.putNumber("distance limelight", getDistance());
+
+    
+      
+
+  }
+
+}
